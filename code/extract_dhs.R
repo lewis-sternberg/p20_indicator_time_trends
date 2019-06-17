d6c660679dca313e13926b4afe47a79f44fb951a
Alex Miller <alex.k.miller@gmail.com>
2019-06-17 09:47:33 -0400

    Extract MIS

--- code/extract_dhs.R
+++ code/extract_dhs.R
@@ -5,21 +5,21 @@ if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
 ####Run function####
 # set our working directory, change this if using on another machine
-wd <- "/home/alex/Survey Microdata/DHSauto"
+wd <- "/home/alex/Survey Microdata/MISauto"
 setwd(wd)
 
 output.dir = "/home/alex/git/p20_private_data/project_data/DHS auto/"
 
 #Unzip
-# message("Unzipping...")
-# zips <- list.files(pattern="*.zip",ignore.case=T,full.names=T)
-# pb = txtProgressBar(max=length(zips),style=3)
-# for(i in 1:length(zips)){
-#   setTxtProgressBar(pb,i)
-#   zip <- zips[i]
-#   unzip(zip)
-# }
-# close(pb)
+message("Unzipping...")
+zips <- list.files(pattern="*.zip",ignore.case=T,full.names=T)
+pb = txtProgressBar(max=length(zips),style=3)
+for(i in 1:length(zips)){
+  setTxtProgressBar(pb,i)
+  zip <- zips[i]
+  unzip(zip)
+}
+close(pb)
 
 too.big = c("iahr74fl","iahr74fl","iapr74fl","iabr74fl","iair74fl","iakr74fl")
 
